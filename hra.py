f4441e569f34710799f671e46d383977f647d1ca
Jakub Chabera <jakub.chabera.021@pslib.cz>
2023-05-14 15:44:45 +0200

    better code

--- hra.py
+++ hra.py
@@ -1,12 +1,80 @@
-#Import veskerych veci
+#Import 
 import pygame
 from sys import exit
 from random import randint, choice
 import random
-
 from pygame.sprite import AbstractGroup
 
+#obecne nastaveni
+pygame.init()
+screen = pygame.display.set_mode((800,400)) # Nastavení velikosti okna
+pygame.display.set_caption('Pixel Run BETA') # Nastavení názvu okna
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf',50) # Přidání fontu
+game_active = False
+start_time = 0
+score = 0
 
+# Hudba
+bg_music = pygame.mixer.Sound('audio/music.wav') # Přidání hudby
+bg_music.play(loops = -1) # Nastavení na loop
+bg_music.set_volume(0.2) # Nastavení hlasitosti
+
+obstacle_group = pygame.sprite.Group()
+
+# Přidání pozadí
+sky_surface = pygame.image.load('graphics/Sky2.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+
+
+# Načtení herní grafiky
+obstacle_rectangle_list = []
+snail_frame_1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha() 
+snail_frame_2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha() 
+snail_frames = [snail_frame_1, snail_frame_2]
+snail_frame_index = 0
+snail_surface = snail_frames[snail_frame_index]
+
+fly_frame_1 = pygame.image.load('graphics/fly/fly1.png').convert_alpha() 
+fly_frame_2 = pygame.image.load('graphics/fly/fly2.png').convert_alpha()
+fly_frames = [fly_frame_1, fly_frame_2]
+fly_frame_index = 0
+fly_surface = fly_frames[fly_frame_index]
+
+player_walk_1 = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha() 
+player_walk_2 = pygame.image.load('graphics/player/player_walk_2.png').convert_alpha() 
+player_walk = [player_walk_1, player_walk_2]
+player_index = 0
+player_jump = pygame.image.load('graphics/Player/jump.png').convert_alpha() 
+
+player_surface = player_walk[player_index]
+player_rectangle = player_surface.get_rect(midbottom = (80,300))
+player_gravity = 0
+
+
+# Úvodní obrazovka
+player_stand = pygame.image.load('graphics/Player/player_stand_red.png').convert_alpha() 
+player_stand = pygame.transform.rotozoom(player_stand, 0,2)
+player_stand_rectangle = player_stand.get_rect(center = (400,200))
+
+game_name = test_font.render('Pixel Run', False, (111,196,169))
+game_name_rectangle = game_name.get_rect(center = (400,80))
+
+game_message = test_font.render('Press space to play', False, (111,196,169))
+game_message_rectangle = game_message.get_rect(center = (400, 330))
+
+# Čas
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer,1500)
+
+snail_animation_timer = pygame.USEREVENT + 2
+pygame.time.set_timer(snail_animation_timer, 500)
+
+fly_animation_timer = pygame.USEREVENT + 3
+pygame.time.set_timer(fly_animation_timer, 200)
+
+
+# Definovat třídu hráče
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -48,6 +116,7 @@ class Player(pygame.sprite.Sprite):
         self.apply_gravity()
         self.animation_state()
 
+# Definice třídy objetků
 class Obstacle(pygame.sprite.Sprite):
     def __init__(self, type):
         super().__init__()
@@ -81,7 +150,7 @@ class Obstacle(pygame.sprite.Sprite):
         if self.rect.x <= -100:
             self.kill()
         
-#zobrazovani skore
+# Zobrazování skóre
 def display_score():
     current_time = int((pygame.time.get_ticks() - start_time) / 1000) #predelani na sekundy
     score_surface = test_font.render(f"Score: {current_time}", False, (64,64,64))
@@ -89,7 +158,7 @@ def display_score():
     screen.blit(score_surface, score_rectangle)
     return current_time
 
-#definovani pohybu objektu
+# Definování pohybu objektů
 def obstacle_movement(obstacle_list):
     if obstacle_list:
         for obstacle_rectangle in obstacle_list:
@@ -103,14 +172,14 @@ def obstacle_movement(obstacle_list):
         return obstacle_list
     else: return []
 
-#definovani kolize
+# Definování kolizí 
 def collisions(player,obstacles):
     if obstacles:
         for obstacle_rectangle in obstacles:
             if player.colliderect(obstacle_rectangle): return False
     return True
 
-#definovani kolize s jakymkoliv jinym spritem
+# Definování kolize s jakýmkoliv jiným Spritem
 def collision_sprite():
     if pygame.sprite.spritecollide(player.sprite, obstacle_group, False):
         obstacle_group.empty()
@@ -118,7 +187,7 @@ def collision_sprite():
     else: 
         return True
 
-#nastavovani animaci
+# Nastavování animací
 def player_animation():
     global player_surface, player_index
     
@@ -129,76 +198,9 @@ def player_animation():
         if player_index >= len(player_walk): player_index = 0
         player_surface = player_walk[int(player_index)]
 
-#obecne nastaveni
-pygame.init()
-screen = pygame.display.set_mode((800,400)) #nastaveni velikosti okna
-pygame.display.set_caption('Pixel Run BETA') #nastaveni nazvu
-clock = pygame.time.Clock()
-test_font = pygame.font.Font('font/Pixeltype.ttf',50) #pridani fontu
-game_active = False
-start_time = 0
-score = 0
-bg_music = pygame.mixer.Sound('audio/music.wav') #pridani hudby
-bg_music.play(loops = -1) #nastaveni na loop
-bg_music.set_volume(0.2) #nastaveni hlasitosti
-
-
-#Skupiny
+# Skupiny
 player = pygame.sprite.GroupSingle()
 player.add(Player())
-
-obstacle_group = pygame.sprite.Group()
-
-#pridani nebe a zeme
-sky_surface = pygame.image.load('graphics/Sky2.png').convert()
-ground_surface = pygame.image.load('graphics/ground.png').convert()
-
-#Snek
-snail_frame_1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha() #alpha pomerne zbytecne ale je to potom vice plynule
-snail_frame_2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha() #alpha pomerne zbytecne ale je to potom vice plynule
-snail_frames = [snail_frame_1, snail_frame_2]
-snail_frame_index = 0
-snail_surface = snail_frames[snail_frame_index]
-
-#Moucha
-fly_frame_1 = pygame.image.load('graphics/fly/fly1.png').convert_alpha() #alpha pomerne zbytecne ale je to potom vice plynule
-fly_frame_2 = pygame.image.load('graphics/fly/fly2.png').convert_alpha() #alpha pomerne zbytecne ale je to potom vice plynule
-fly_frames = [fly_frame_1, fly_frame_2]
-fly_frame_index = 0
-fly_surface = fly_frames[fly_frame_index]
-
-obstacle_rectangle_list = []
-
-player_walk_1 = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha() #alpha pomerne zbytecne ale je to potom vice plynule
-player_walk_2 = pygame.image.load('graphics/player/player_walk_2.png').convert_alpha() #alpha pomerne zbytecne ale je to potom vice plynule
-player_walk = [player_walk_1, player_walk_2]
-player_index = 0
-player_jump = pygame.image.load('graphics/Player/jump.png').convert_alpha() #alpha pomerne zbytecne ale je to potom vice plynule
-
-player_surface = player_walk[player_index]
-player_rectangle = player_surface.get_rect(midbottom = (80,300))
-player_gravity = 0
-
-#Uvodni obrazovka
-player_stand = pygame.image.load('graphics/Player/player_stand_red.png').convert_alpha() #alpha pomerne zbytecne ale je to potom vice plynule
-player_stand = pygame.transform.rotozoom(player_stand, 0,2)
-player_stand_rectangle = player_stand.get_rect(center = (400,200))
-
-game_name = test_font.render('Pixel Run', False, (111,196,169))
-game_name_rectangle = game_name.get_rect(center = (400,80))
-
-game_message = test_font.render('Press space to play', False, (111,196,169))
-game_message_rectangle = game_message.get_rect(center = (400, 330))
-
-#Cas
-obstacle_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(obstacle_timer,1500)
-
-snail_animation_timer = pygame.USEREVENT + 2
-pygame.time.set_timer(snail_animation_timer, 500)
-
-fly_animation_timer = pygame.USEREVENT + 3
-pygame.time.set_timer(fly_animation_timer, 200)
 
 while True:
     for event in pygame.event.get():
@@ -226,18 +228,18 @@ while True:
         screen.blit(ground_surface,(0,300))
         score = display_score()
 
-        #Vykreslovani hrace
+        #Vykreslování hráče
         player.draw(screen)
         player.update()
         
-        #Vykreslovani objektu
+        # Vykreslování objektů
         obstacle_group.draw(screen)
         obstacle_group.update()
 
-        #Kolize
+        # Kolize
         game_active = collision_sprite()
         
-    #END obrazovka
+    # END obrazovka
     else:
         screen.fill((94,129,162))
         screen.blit(player_stand, player_stand_rectangle)
@@ -254,6 +256,6 @@ while True:
         if score == 0: screen.blit(game_message, game_message_rectangle)
         else: screen.blit(score_message, score_message_rect)
         
-    #nastaveni obnovovani obrazovky (fps)  
+    # Nastavení obnovovací obrazovky (fps)  
     pygame.display.update()
     clock.tick(60)
